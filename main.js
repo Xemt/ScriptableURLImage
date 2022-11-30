664deaae94e77e6595d7dc8370f23fa94ae2dcb9
Xemt <92655476+Xemt@users.noreply.github.com>
2022-11-30 15:01:27 -0600

    More Changes
    More minifying/obsfuscating. Changed some var names.

--- main.js
+++ main.js
@@ -3,15 +3,16 @@
  - await URLImage.get(url: String): Image, await URLImage.getUnsafe(url: String): Image
  - Also, this was made by Xemt on GitHub and blah blah blah
 */
+var iE="The image wasn't retrieved YET (most likely), or the image type is invalid (less likely?)"
 const URLImage = new Proxy({
  image: null,
  url: null,
- save: function(){if(new.target){throw"Do not call this function using 'new'"}else{let i=this.image;(i&&i instanceof Image)?Photos.save(i):null}}, // save
- toBase64: function(){if(new.target){throw"Do not call this function using 'new'"}else{let i=this.image;if(i&&i instanceof Image){let j=Data.fromJPEG(i);let p=Data.fromPNG(i);j?i=j.toBase64String():p?i=p.toBase64String():0;return i}else logError("The image wasn't retrieved YET (most likely), or the image type is invalid (less likely?)")}}, // toBase64
- toRawString: function(){if(new.target){throw"Do not call this function using 'new'"}else{let i=this.image;if(i&&i instanceof Image){let j=Data.fromJPEG(i);let p=Data.fromPNG(i);if(j)i=j.toRawString();else if(p)i=p.toRawString();return i}else logError("The image wasn't retrieved YET (most likely), or the image type is invalid (less likely?)")}}, // toRawString
- toBytes: function(){if(new.target){throw"Do not call this function using 'new'"}else{let i=this.image;if(i&&i instanceof Image){let j=Data.fromJPEG(i);let p=Data.fromPNG(i);j?i=j.getBytes():p?i=p.getBytes():0;return i}else logError("The image wasn't retrieved YET (most likely), or the image type is invalid (less likely?)")}}, // toBytes
- get: async function(url){if(new.target)throw"Do not call this function using 'new'";else{if(url&&typeof url==="string"&&url.match(/^https:\/\//g)){this.url=url;await new Request(url).loadImage().then(i=>{this.image=i},r=>{throw r});return this.image}else{throw"URL is invalid"}}}, // getSafe
- getUnsafe: async function(url){if(new.target)throw"Do not call this function using 'new'";else{if(url&&typeof url==="string"){this.url=url;let q=new Request(url);q.allowInsecureRequest=true;await q.loadImage().then(i=>{this.image=i},r=>{throw r});return this.image}else{throw"URL is invalid"}}}, // getUnsafe
+ save: function(){if(new.target){throw"Do not call this function using 'new'"}else{let a=this.image;if(a&&a instanceof Image)Photos.save(a);else logError(mE)}}, // save
+ toBase64: function(){if(new.target){throw"Do not call this function using 'new'"}else{let a=this.image;if(a&&a instanceof Image){let b=Data.fromJPEG(a);let c=Data.fromPNG(a);b?a=b.toBase64String():c?a=c.toBase64String():0;return a}else logError(iE)}}, // toBase64
+ toRawString: function(){if(new.target){throw"Do not call this function using 'new'"}else{let a=this.image;if(a&&a instanceof Image){let b=Data.fromJPEG(a);let c=Data.fromPNG(a);a=(b?b.toRawString():c?c.toRawString():0);return a}else logError(iE)}}, // toRawString
+ toBytes: function(){if(new.target){throw"Do not call this function using 'new'"}else{let a=this.image;if(a&&a instanceof Image){let b=Data.fromJPEG(a);let c=Data.fromPNG(a);a=(b?b.getBytes():c?c.getBytes():0);return a}else logError(iE)}}, // toBytes
+ get: async function(a){if(new.target)throw"Do not call this function using 'new'";else{if(a&&typeof a==="string"&&a.match(/^https:\/\//g)){this.url=a;await new Request(a).loadImage().then(b=>{this.image=b},c=>{throw c});return this.image}else{throw"URL is invalid"}}}, // getSafe
+ getUnsafe: async function(a){if(new.target)throw"Do not call this function using 'new'";else{if(a&&typeof a==="string"){this.url=a;let b=new Request(a);b.allowInsecureRequest=!!0;await b.loadImage().then(c=>{this.image=c},d=>{throw d});return this.image}else{throw"URL is invalid"}}}, // getUnsafe
 }, {
- set: function(o,p,v){if(p==="image"&&v&&v instanceof Image||p==="url"&&v&&typeof v==="string")o[p]=v;else if(p==="image"&&(v instanceof Image)===false||p==="url"&&(v instanceof String)===false)logError(`Invalid value for URLImage.${p}`);else if(p!=="image"&&p!=="url")logError(`cannot change this value (${"URLImage." + p})`)}
+ set: function(a,b,c){if(b==="image"&&c&&c instanceof Image||b==="url"&&c&&typeof c==="string")a[b]=c;else if(b==="image"&&(c instanceof Image)===!1||b==="url"&&(c instanceof String)===!1)logError(`Invalid value for URLImage.${b}`);else if(b!=="image"&&b!=="url")logError(`cannot change this value (${"URLImage."+b})`)}
 })
